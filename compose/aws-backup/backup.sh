60ce526cb897245894587ca252f0cd6a83fa1162
Jordan Reedie <jjreedie@gmail.com>
2018-07-24 14:02:16 -0400

    change file name format, upload both data and logs

--- compose/aws-backup/backup.sh
+++ compose/aws-backup/backup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd /
 echo "compressing /data"
-tar -zcf tmp/$HOSTNAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz data/data
+tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_data.tar.gz /data
 echo "compressing /logs"
-tar -zcf tmp/$HOSTNAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz data/logs
+tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_logs.tar.gz /logs
 echo "uploading tarballs"
-/usr/local/bin/aws s3 cp /tmp/*.tar.gz $AWS_S3_BUCKET_URL
+/usr/local/bin/aws s3 cp /tmp $AWS_S3_BUCKET_URL --recursive --exclude "*" --include "*.tar.gz"
 echo "cleaning up"
 rm /tmp/*.tar.gz
+
--- compose/aws-backup/entrypoint.sh
+++ compose/aws-backup/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "populating necessary cron env vars"
 printenv | grep "TZ\|AWS\|HOSTNAME" >> /etc/environment
 echo "creating crontab"
-crontab -l 2>/dev/null | { echo "$CRON_SCHEDULE /backup.sh > /tmp/testout 2>&1 \n"; } | crontab -
+crontab -l 2>/dev/null | echo "$CRON_SCHEDULE /backup.sh > /tmp/testout 2>&1 \n" | crontab -
 echo "verify crontab:"
 crontab -l
 echo "starting cron"
