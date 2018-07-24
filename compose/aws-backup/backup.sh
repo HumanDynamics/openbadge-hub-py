5cb840af82f6c44d2d31a26c35c334a747f9a806
Oren Lederman <oren.lederman@gmail.com>
2018-07-24 14:19:45 -0400

    Removing leading / from path

--- compose/aws-backup/backup.sh
+++ compose/aws-backup/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /
 echo "compressing /data"
-tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_data.tar.gz /data
+tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_data.tar.gz data
 echo "compressing /logs"
-tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_logs.tar.gz /logs
+tar -zcf tmp/$HOSTNAME-`date "+%Y%m%d%H%M%S"`_logs.tar.gz logs
 echo "uploading tarballs"
 /usr/local/bin/aws s3 cp /tmp $AWS_S3_BUCKET_URL --recursive --exclude "*" --include "*.tar.gz"
 echo "cleaning up"
